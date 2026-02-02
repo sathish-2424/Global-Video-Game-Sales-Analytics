51cf09843763ed459c860255e490fa43a9865abf
sathish-2424 <sathish9268@gmail.com>
2026-02-02 10:24:41 +0530

    'updat'

--- game_sales_analysis.sql
+++ game_sales_analysis.sql
@@ -1,60 +1,111 @@
-select * from game_sales;
+CREATE DATABASE `Parks_and_Recreation`;
+USE `Parks_and_Recreation`;
 
-SELECT COUNT(*) AS total_games
-FROM game_sales;
 
-SELECT game, global
-FROM game_sales
-ORDER BY global DESC
-LIMIT 10;
+CREATE TABLE employee_demographics (
+  employee_id INT NOT NULL,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  age INT,
+  gender VARCHAR(10),
+  birth_date DATE,
+  PRIMARY KEY (employee_id)
+);
 
-SELECT genre,
-       ROUND(SUM(global),2) AS total_sales
-FROM game_sales
-GROUP BY genre
-ORDER BY total_sales DESC;
+CREATE TABLE employee_salary (
+  employee_id INT NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  occupation VARCHAR(50),
+  salary INT,
+  dept_id INT
+);
 
-SELECT platform,
-       ROUND(SUM(global),2) AS global_sales
-FROM game_sales
-GROUP BY platform
-ORDER BY global_sales DESC;
+CREATE TABLE parks_departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name varchar(50) NOT NULL,
+  PRIMARY KEY (department_id)
+);
 
-SELECT publisher,
-       COUNT(*) AS total_games,
-       ROUND(SUM(global),2) AS total_sales
-FROM game_sales
-GROUP BY publisher
-ORDER BY total_sales DESC;
 
-SELECT dominant_region,
-       COUNT(*) AS games_count
-FROM game_sales
-GROUP BY dominant_region;
+INSERT INTO employee_demographics (employee_id, first_name, last_name, age, gender, birth_date)
+VALUES
+(1,'Leslie', 'Knope', 44, 'Female','1979-09-25'),
+(3,'Tom', 'Haverford', 36, 'Male', '1987-03-04'),
+(4, 'April', 'Ludgate', 29, 'Female', '1994-03-27'),
+(5, 'Jerry', 'Gergich', 61, 'Male', '1962-08-28'),
+(6, 'Donna', 'Meagle', 46, 'Female', '1977-07-30'),
+(7, 'Ann', 'Perkins', 35, 'Female', '1988-12-01'),
+(8, 'Chris', 'Traeger', 43, 'Male', '1980-11-11'),
+(9, 'Ben', 'Wyatt', 38, 'Male', '1985-07-26'),
+(10, 'Andy', 'Dwyer', 34, 'Male', '1989-03-25'),
+(11, 'Mark', 'Brendanawicz', 40, 'Male', '1983-06-14'),
+(12, 'Craig', 'Middlebrooks', 37, 'Male', '1986-07-27');
 
-SELECT game, platform, europe
-FROM game_sales
-WHERE dominant_region = 'Europe'
-ORDER BY europe DESC;
 
-SELECT sales_tier,
-       COUNT(*) AS games
-FROM game_sales
-GROUP BY sales_tier;
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES
+(1, 'Leslie', 'Knope', 'Deputy Director of Parks and Recreation', 75000,1),
+(2, 'Ron', 'Swanson', 'Director of Parks and Recreation', 70000,1),
+(3, 'Tom', 'Haverford', 'Entrepreneur', 50000,1),
+(4, 'April', 'Ludgate', 'Assistant to the Director of Parks and Recreation', 25000,1),
+(5, 'Jerry', 'Gergich', 'Office Manager', 50000,1),
+(6, 'Donna', 'Meagle', 'Office Manager', 60000,1),
+(7, 'Ann', 'Perkins', 'Nurse', 55000,4),
+(8, 'Chris', 'Traeger', 'City Manager', 90000,3),
+(9, 'Ben', 'Wyatt', 'State Auditor', 70000,6),
+(10, 'Andy', 'Dwyer', 'Shoe Shiner and Musician', 20000, NULL),
+(11, 'Mark', 'Brendanawicz', 'City Planner', 57000, 3),
+(12, 'Craig', 'Middlebrooks', 'Parks Director', 65000,1);
 
-SELECT genre, platform,
-       ROUND(AVG(global),2) AS avg_global_sales
-FROM game_sales
-WHERE sales_tier = 'Blockbuster'
-GROUP BY genre, platform
-ORDER BY avg_global_sales DESC;
 
-SELECT game, genre, global,
-       RANK() OVER (PARTITION BY genre ORDER BY global DESC) AS rank_in_genre
-FROM game_sales;
+INSERT INTO parks_departments (department_name)
+VALUES
+('Parks and Recreation'),
+('Animal Control'),
+('Public Works'),
+('Healthcare'),
+('Library'),
+('Finance');
 
-SELECT year,
-       ROUND(SUM(global),2) AS yearly_sales
-FROM game_sales
-GROUP BY year
-ORDER BY year;
+###select clause
+
+select * from employee_demographics;
+
+select first_name from employee_demographics;
+
+select first_name,last_name,salary,salary+100 as new_salary from employee_salary;
+
+SELECT distinct dept_id FROM employee_salary;
+
+###where clause
+
+select * from employee_salary where salary >50000;
+
+select * from employee_demographics where gender != 'female';
+
+select * from employee_demographics where birth_date > '1979-09-25';
+
+select * from employee_demographics where first_name like '__a%';
+
+###Group by & Order by
+
+select gender,count(gender) as Gender_count from employee_demographics group by gender;
+
+SELECT occupation, salary FROM employee_salary GROUP BY occupation, salary;
+
+select gender,age from employee_demographics where gender ='female' order by gender desc,age desc;
+
+select gender,age from employee_demographics where gender ='female' ;
+
+###where or having
+
+SELECT gender, AVG(age)
+FROM employee_demographics
+WHERE age > 40
+GROUP BY gender;
+
+select gender, avg(age) from employee_demographics group by gender having AVG(age) >40;
+
+
+
